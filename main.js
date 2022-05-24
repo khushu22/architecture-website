5a9758b3c1b248408a7d15f3a4c661b310db69ae
khushu22 <104973882+khushu22@users.noreply.github.com>
2022-05-24 18:01:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,116 @@
+function items(){
+    if(myitems.style.display!='block'){
+        myitems.style.display='block'
+    }
+
+    else{
+        myitems.style.display='none'
+    }
+}
+
+
+function hover(){
+    arch.style.color='white'
+    lor.style.color='white'
+    sp.style.display='inline'
+   
+
+}
+
+function nohover(){
+    arch.style.color='black'
+    lor.style.color='rgb(115, 117, 118)'
+    sp.style.display='none'
+   
+
+}
+
+
+function hover2(){
+    arch2.style.color='white'
+    lor2.style.color='white'
+    sp2.style.display='inline'
+   
+   
+
+}
+
+function nohover2(){
+    arch2.style.color='black'
+    lor2.style.color='rgb(115, 117, 118)'
+    sp2.style.display='none'
+
+}
+
+
+function hover3(){
+    arch3.style.color='white'
+    lor3.style.color='white'
+    sp3.style.display='inline'
+   
+   
+
+}
+
+function nohover3(){
+    arch3.style.color='black'
+    lor3.style.color='rgb(115, 117, 118)'
+    sp3.style.display='none'
+
+}
+
+
+function hover4(){
+    arch4.style.color='white'
+    lor4.style.color='white'
+    sp4.style.display='inline'
+   
+   
+
+}
+
+function nohover4(){
+    arch4.style.color='black'
+    lor4.style.color='rgb(115, 117, 118)'
+    sp4.style.display='none'
+   
+
+}
+
+
+
+
+function hover5(){
+    arch5.style.color='white'
+    lor5.style.color='white'
+    sp5.style.display='inline'
+   
+
+}
+
+function nohover5(){
+    arch5.style.color='black'
+    lor5.style.color='rgb(115, 117, 118)'
+    sp5.style.display='none'
+   
+
+}
+
+
+function hover6(){
+    arch6.style.color='white'
+    lor6.style.color='white'
+    sp6.style.display='inline'
+   
+   
+
+}
+
+function nohover6(){
+    arch6.style.color='black'
+    lor6.style.color='rgb(115, 117, 118)'
+    sp6.style.display='none'
+
+}
+
+
